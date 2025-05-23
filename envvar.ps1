f22833998fad98c2363a84b8f2cd2403cd117107
Kasper1985 <Kasper1985@gmail.com>
2025-05-23 10:19:33 +0200

    Remove unnecessary script

--- envvar.ps1
+++
@@ -1,5 +0,0 @@
-Get-Content .env | ForEach-Object {
-    if ($_ -match '^(.*?)=(.*)$') {
-        [System.Environment]::SetEnvironmentVariable($matches[1].Trim(), $matches[2].Trim(), [System.EnvironmentVariableTarget]::Process)
-    }
-}
